05bde0ea0b7620cd7231c8dc15ad41f6cac684ad
你手捧鲜花 <157215725@qq.com>
2021-06-17 14:55:57 +0800

    opz: signer logic

--- src/Contracts/SignerInterface.php
+++ src/Contracts/SignerInterface.php
@@ -9,16 +9,18 @@ interface SignerInterface
 {
     /**
      * @param array $params
+     * @param string|null $secretKey
      *
      * @return string
      */
-    public function sign(array $params): string;
+    public function sign(array $params, ?string $secretKey = null): string;
 
     /**
      * @param string $signature
      * @param array $params
+     * @param string|null $secretKey
      *
      * @return bool
      */
-    public function verify(string $signature, array $params): bool;
+    public function verify(string $signature, array $params, ?string $secretKey = null): bool;
 }
--- src/Signer.php
+++ src/Signer.php
@@ -20,43 +20,106 @@ abstract class Signer implements SignerInterface
     {
         $this->app = $app;
     }
-    
+
     /**
      * @param array $params
      *
-     * @return string
+     * @return array
      */
-    public function sign(array $params): string
+    protected function filterParams(array $params): array
     {
-        $params = array_filter($params, function ($param) {
+        return array_filter($params, function ($param) {
             return $param !== null && $param !== '';
         });
+    }
 
-        $params = array_map(function ($param) {
+
+    /**
+     * @param array $params
+     *
+     * @return array|string[]
+     */
+    protected function formatParams(array $params): array
+    {
+        return array_map(function ($param) {
             if (is_array($param)) {
                 return json_encode($param, JSON_UNESCAPED_UNICODE);
             }
             return $param;
         }, $params);
+    }
 
+    /**
+     * @param array $params
+     *
+     * @return array
+     */
+    protected function sortParams(array $params): array
+    {
         ksort($params);
 
-        foreach ($params as $k => $v) {
-            $sign[] = "{$k}={$v}";
-        }
-        $sign = join('&', $sign);
+        return $params;
+    }
 
-        return strtoupper(md5($sign));
+    /**
+     * @param array $params
+     * @param string|null $secretKey
+     *
+     * @return string
+     */
+    protected function joinParams(array $params, ?string $secretKey = null): string
+    {
+        $secretKey = $secretKey ?: '';
+
+        $stringToBeSigned = $secretKey;
+
+        foreach ($params as $k => $v) {
+            $stringToBeSigned .= "{$k}{$v}";
+        }
+
+        $stringToBeSigned .= $secretKey;
+
+        return $stringToBeSigned;
+    }
+
+    /**
+     * @param string $stringToBeSigned
+     *
+     * @return string
+     */
+    protected function encryptString(string $stringToBeSigned): string
+    {
+        return strtoupper(md5($stringToBeSigned));
+    }
+
+    /**
+     * @param array $params
+     * @param string|null $secretKey
+     *
+     * @return string
+     */
+    public function sign(array $params, ?string $secretKey = null): string
+    {
+        $params = $this->filterParams($params);
+
+        $params = $this->formatParams($params);
+
+        $params = $this->sortParams($params);
+
+        $stringToBeSigned = $this->joinParams($params, $secretKey);
+
+        return $this->encryptString($stringToBeSigned);
     }
 
     /**
      * @param string $signature
      * @param array $params
+     * @param string|null $secretKey
      *
      * @return bool
      */
-    public function verify(string $signature, array $params): bool
+    public function verify(string $signature, array $params, ?string $secretKey = null): bool
     {
-        return $signature == $this->sign($params);
+        return $signature == $this->sign($params, $secretKey);
     }
 }
